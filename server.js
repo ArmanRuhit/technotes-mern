1e0d6dd2aa1d1d0bbfffb605a030a2dc08499a37
ArmanRuhit <armanruhit09@gmail.com>
2024-06-19 22:33:25 +0600

    Chapter-3
    Connected to mongodb server

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
+require('dotenv').config();
 const express = require("express");
 const path = require('path');
 const app = express();
-const {logger} = require('./middleware/logger');
+const {logger, logEvents} = require('./middleware/logger');
 const PORT = process.env.PORT || 3500;
 const errorHandler = require('./middleware/errorHandler');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const corsOptions = require('./config/corsOptions');
+const connectDB = require('./config/dbConnection');
+const mongoose = require('mongoose');
 
+connectDB();
 app.use(logger);
 app.use(cors(corsOptions));
+app.use(express.json()); //this will help process json
 app.use(cookieParser());
 app.use('/', express.static(path.join(__dirname, 'public')));
 // app.use(express.static(path.join('public'))); this will also work
@@ -27,4 +32,13 @@ app.all('*', (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`Server listening at http://localhost:${PORT}`))
+
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => console.log(`Server listening at http://localhost:${PORT}`))
+})
+
+mongoose.connection.on('error', err => {
+    console.log(err);
+    logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log');
+})
