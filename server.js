bd8c8ac5507752294e344ddb8eed706559cc5c00
ArmanRuhit <armanruhit09@gmail.com>
2024-06-19 16:24:02 +0600

    Chapter-3 Done
    Implemented Custom Middleware

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const path = require('path');
 const app = express();
+const {logger} = require('./middleware/logger');
 const PORT = process.env.PORT || 3500;
+const errorHandler = require('./middleware/errorHandler');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const corsOptions = require('./config/corsOptions');
 
-
-app.use('/', express.static(path.join(__dirname, '/public')));
+app.use(logger);
+app.use(cors(corsOptions));
+app.use(cookieParser());
+app.use('/', express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join('public'))); this will also work
 app.use('/', require('./routes/root'));
 app.all('*', (req, res) => {
     res.status(404);
@@ -16,5 +24,7 @@ app.all('*', (req, res) => {
         res.type('txt').send('404 not found');
     }
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server listening at http://localhost:${PORT}`))
