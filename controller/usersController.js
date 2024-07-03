e966094030e558c4581825e158f18dfb7f59017d
arman <armanruhit09@gmail.com>
2024-07-03 16:30:13 +0600

    Update usersController.js

--- controller/usersController.js
+++ controller/usersController.js
@@ -3,6 +3,8 @@ const Note = require('../models/Note');
 const expressAsyncHandler = require('express-async-handler');
 const bcrypt = require('bcrypt');
 
+const salt = 10;
+
 //@desc Get All Users
 //@route Get /users
 //@acess Private
@@ -34,7 +36,7 @@ const createNewUser = expressAsyncHandler(async (req, res) => {
     }
 
     //hash password
-    const hashPwd = await bcrypt.hash(password, 10) //salt round
+    const hashPwd = await bcrypt.hash(password, salt) //salt round
 
     const userObj = { user, "password": hashPwd, roles };
 
@@ -51,14 +53,64 @@ const createNewUser = expressAsyncHandler(async (req, res) => {
 //@route Patch /users
 //@acess Private
 const updateUser = expressAsyncHandler(async (req, res) => {
-    
+    const { id, username, roles, active, password } = req.body;
+
+
+    //confirm data
+    if (!id || !username || !Array.isArray(roles) || !roles.length || typeof active === 'boolean') {
+        return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const user = await User.findById(id).exec();
+
+    if (!user) {
+        return res.status(404).json({ message: "User not found" });
+    }
+
+    const duplicate = await User.findOne({ username }).lean().exec();
+
+    if (duplicate && duplicate?._id.toString() === id) {
+        return res.status(409).json({ message: "Duplicate username" });
+    }
+
+    user.username = username;
+    user.active = active;
+    user.roles = roles;
+
+    if (password) {
+        user.password = await bcrypt.hash(password, salt);
+    }
+
+    const updateUser = await user.save();
+
+    res.json({ message: `${updateUser.username} updated` });
 });
 
 //@desc Delete a User
 //@route Delete /users
 //@acess Private
 const deleteUser = expressAsyncHandler(async (req, res) => {
+    const { id } = req.body;
+
+    if (!id) {
+        return res.status(400).json({ message: "All fields are required" });
+    }
     
+    const notes = await Note.findOne({ user: id }).lean().exec();
+
+    if (notes?.length) {
+        return res.status(400).({ message: "User has assigned notes" });
+    }
+
+    const user = await User.findById(id).exec();
+
+    if (!user) {
+        return res.status(404).json({ message: "User not found" });
+    }
+
+    const result = await user.deleteOne();
+
+    const reply = `Username ${result.username} `
 });
 
 module.exports = {
